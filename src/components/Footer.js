bc92943439fe77e5b8bcd55e40ce197c059d58e7
AlittleJuku <109045201+amashriq@users.noreply.github.com>
2024-08-04 13:26:47 -0400

    Update package-lock, package, and Footer components

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,6 +3,8 @@ import { Link } from 'react-router-dom'
 import '../App.css'
 import './Footer.css'
 import Logo from '../assets/images/oc_logo-inv-edit.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faInstagram, faFacebook } from '@fortawesome/free-brands-svg-icons'
 
 const Footer = () => {
   return (
@@ -13,12 +15,15 @@ const Footer = () => {
         </Link> 
 
         <div className="footer-right">
-          <h2 className="footer-title">Contact Us</h2>
+          <h2 className="footer-title">Socials</h2>
           <div className="divider"></div>
           <div className="footer-text">
-            Socials: <br />
-            <a className="footer-link" href="https://www.instagram.com/ocvball" target="_blank" rel="noopener noreferrer">Instagram</a> <br />
-            <a className="footer-link" href="https://www.facebook.com/ocvball" target="_blank" rel="noopener noreferrer">Facebook</a>
+            <a className="footer-link" href="https://www.instagram.com/ocvball" target="_blank" rel="noopener noreferrer">
+              <FontAwesomeIcon icon={faInstagram} size="2x" className = "InstaIcon" />
+            </a>
+            <a className="footer-link" href="https://www.facebook.com/ocvball" target="_blank" rel="noopener noreferrer">
+              <FontAwesomeIcon icon={faFacebook} size="2x" className = "FbIcon" />
+            </a>
           </div>
         </div>
       </div>
